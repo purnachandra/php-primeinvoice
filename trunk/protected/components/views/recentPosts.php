e9a5750f7845f018f9cd18efb6b26064c7080ff2
mocapapa@g.pugpug.org <mocapapa@g.pugpug.org@fbabf7d8-ffad-11dd-802b-177316e4ac06>
2009-03-19 04:03:57 +0000

    Added createTime for "Recent Posts" widget.

--- trunk/protected/components/views/recentPosts.php
+++ trunk/protected/components/views/recentPosts.php
@@ -2,6 +2,8 @@
 <?php foreach($this->getRecentPosts() as $post): ?>
 <li>
 <?php echo CHtml::link(CHtml::encode($post->title),array('post/show','id'=>$post->id)); ?>
+&nbsp;on&nbsp;
+<?php echo CHTml::link(date('M j', $post->createTime), array('post/PostedOnDate', 'time'=>$post->createTime)); ?>
 </li>
 <?php endforeach; ?>
 </ul>
