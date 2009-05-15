3b18c49122c2983cd6def339e588a9b1d5c5e64a
mocapapa <mocapapa@g.pugpug.org>
2009-05-15 09:06:00 +0000

    Added site-wide search portlet

--- protected/views/post/list.php
+++ protected/views/post/list.php
@@ -1,6 +1,9 @@
 <?php if(!empty($_GET['tag'])): ?>
 <h3>Posts Tagged with "<?php echo CHtml::encode($_GET['tag']); ?>"</h3>
 <?php endif; ?>
+<?php if(!empty($search->string)): ?>
+<h3>Posts Searched by "<?php echo CHtml::encode($search->string); ?>"</h3>
+<?php endif; ?>
 
 <?php foreach($posts as $post): ?>
 <?php $this->renderPartial('_post',array(
