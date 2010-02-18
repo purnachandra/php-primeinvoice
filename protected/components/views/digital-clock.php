9a0ee1f2a80d09b6d116fe3099ec2e752ff7df40
mocapapa <mocapapa@g.pugpug.org>
2010-02-18 03:52:17 +0000

    changed digital clock algorithm

--- protected/components/views/digital-clock.php
+++ protected/components/views/digital-clock.php
@@ -1,6 +1,9 @@
 <center>
 <!-- $Id$ -->
 <?php
+$cs=Yii::app()->clientScript;
+$cs->registerCoreScript('jquery');
+$cs->registerScriptFile(Yii::app()->request->baseUrl.'/js/jquery.delay.js', CClientScript::POS_HEAD);
 $clockOffsetHx = 33;
 $clockOffsetMx = $clockOffsetHx + 42;
 $clockOffsetSx = $clockOffsetMx + 42;
@@ -19,164 +22,77 @@ $clockOffsetYL = $clockOffsetYU + 13;
 <script type="text/javascript">
 /*<![CDATA[*/
    $(document).ready(function() {
-       var static = /(^o^)/;
-       var ch, cm, cs;
+       var ch, cm, cs, ph, pm, ps;
        var currentTime = new Date();
-       if (!('ph' in static)) static.ph = (currentTime.getHours() + 23)%24;
-       if (!('pm' in static)) static.pm = (currentTime.getMinutes() + 59)%60;
-       if (!('ps' in static)) static.ps = (currentTime.getSeconds() + 59)%60;
+       if (ph == undefined) ph = (currentTime.getHours() + 23)%24;
+       if (pm == undefined) pm = (currentTime.getMinutes() + 59)%60;
+       if (ps == undefined) ps = (currentTime.getSeconds() + 59)%60;
 
-       animation0();
+       animation();
        setInterval(function(){
-	   animation0();
+	   animation();
        }, 1000);
 
-       function animationH1u() {
-	 clearTimeout();
-	 $('.hourNumberU').css('background-position', -static.ph*34+'px -26px'); // 1u
-       }
-       function animationM1u() {
-	 clearTimeout();
-	 $('.minuteNumberU').css('background-position', -static.pm*34+'px -26px'); // 1u
-       }
-       function animationS1u() {
-	 clearTimeout();
-	 $('.secondNumberU').css('background-position', -static.ps*34+'px -26px'); // 1u
-       }
-       function animationH2u() {
-	 clearTimeout();
-	 $('.hourNumberU').css('background-position', -static.ph*34+'px -39px'); // 2u
-       }
+       function n1u(t, x) $(t).css('background-position', -x*34+'px -26px'); // 1u
+       function n2u(t, x) $(t).css('background-position', -x*34+'px -39px'); // 2u
+       function n3u(t, x) $(t).css('background-position', -x*34+'px -52px'); // 3u
+       function n4u(t, x) $(t).css('background-position', -x*34+'px 0px'); // 4u
+       function n5l(t, x) $(t).css('background-position', -x*34+'px -65px'); // 5l
+       function n6l(t, x) $(t).css('background-position', -x*34+'px -78px'); // 6l
+       function n7l(t, x) $(t).css('background-position', -x*34+'px -91px'); // 7l
+       function n8l(t, x) $(t).css('background-position', -x*34+'px -13px'); // 8l
 
-       function animationM2u() {
-	 clearTimeout();
-	 $('.minuteNumberU').css('background-position', -static.pm*34+'px -39px'); // 2u
-       }
-       function animationS2u() {
-	 clearTimeout();
-	 $('.secondNumberU').css('background-position', -static.ps*34+'px -39px'); // 2u
-       }
-
-       function animationH3u() {
-	 clearTimeout();
-	 $('.hourNumberU').css('background-position', -static.ph*34+'px -52px'); // 3u
-       }
-       function animationM3u() {
-	 clearTimeout();
-	 $('.minuteNumberU').css('background-position', -static.pm*34+'px -52px'); // 3u
-       }
-       function animationS3u() {
-	 clearTimeout();
-	 $('.secondNumberU').css('background-position', -static.ps*34+'px -52px'); // 3u
-       }
-
-       function animationH4u() {
-	 clearTimeout();
-	 $('.hourNumberU').css('background-position', -ch*34+'px 0px'); // 4u
-       }
-       function animationM4u() {
-	 clearTimeout();
-	 $('.minuteNumberU').css('background-position', -cm*34+'px 0px'); // 4u
-       }
-       function animationS4u() {
-	 clearTimeout();
-	 $('.secondNumberU').css('background-position', -cs*34+'px 0px'); // 4u
-       }
-
-       function animationH5l() {
-	 clearTimeout();
-	 $('.hourNumberL').css('background-position', -static.ph*34+'px -65px'); // 5l
-       }
-       function animationM5l() {
-	 clearTimeout();
-	 $('.minuteNumberL').css('background-position', -static.pm*34+'px -65px'); // 5l
-       }
-       function animationS5l() {
-	 clearTimeout();
-	 $('.secondNumberL').css('background-position', -static.ps*34+'px -65px'); // 5l
-       }
-
-       function animationH6l() {
-	 clearTimeout();
-	 $('.hourNumberL').css('background-position', -static.ph*34+'px -78px'); // 6l
-       }
-       function animationM6l() {
-	 clearTimeout();
-	 $('.minuteNumberL').css('background-position', -static.pm*34+'px -78px'); // 6l
-       }
-       function animationS6l() {
-	 clearTimeout();
-	 $('.secondNumberL').css('background-position', -static.ps*34+'px -78px'); // 6u
-       }
-
-       function animationH7l() {
-	 clearTimeout();
-	 $('.hourNumberL').css('background-position', -static.ph*34+'px -91px'); // 7l
-       }
-       function animationM7l() {
-	 clearTimeout();
-	 $('.minuteNumberL').css('background-position', -static.pm*34+'px -91px'); // 7l
-       }
-       function animationS7l() {
-	 clearTimeout();
-	 $('.secondNumberL').css('background-position', -static.ps*34+'px -91px'); // 7l
-       }
-       
-       function animationH8l() {
-	 clearTimeout();
-	 $('.hourNumberL').css('background-position', -ch*34+'px -13px'); // 8l
-       }
-       function animationM8l() {
-	 clearTimeout();
-	 $('.minuteNumberL').css('background-position', -cm*34+'px -13px'); // 8l
-       }
-       function animationS8l() {
-	 clearTimeout();
-	 $('.secondNumberL').css('background-position', -cs*34+'px -13px'); // 8l
-       }
-       
-       function animation0() {
+       function animation() {
 	 currentTime = new Date();
 	 ch = currentTime.getHours();
 	 cm = currentTime.getMinutes();
 	 cs = currentTime.getSeconds();
 
-	 var duration = 40;
-	 if (ch != static.ph) {
-	   setTimeout(animationH1u, duration);
-	   setTimeout(animationH2u, duration*2);
-	   setTimeout(animationH3u, duration*3);
-	   setTimeout(animationH4u, duration*4);
-	   setTimeout(animationH5l, duration*5);
-	   setTimeout(animationH6l, duration*6);
-	   setTimeout(animationH7l, duration*7);
-	   setTimeout(animationH8l, duration*8);
-	 }
-	 if (cm != static.pm) {
-	   setTimeout(animationM1u, duration);
-	   setTimeout(animationM2u, duration*2);
-	   setTimeout(animationM3u, duration*3);
-	   setTimeout(animationM4u, duration*4);
-	   setTimeout(animationM5l, duration*5);
-	   setTimeout(animationM6l, duration*6);
-	   setTimeout(animationM7l, duration*7);
-	   setTimeout(animationM8l, duration*8);
-	 }
-	 if (cs != static.ps) {
-	   setTimeout(animationS1u, duration);
-	   setTimeout(animationS2u, duration*2);
-	   setTimeout(animationS3u, duration*3);
-	   setTimeout(animationS4u, duration*4);
-	   setTimeout(animationS5l, duration*5);
-	   setTimeout(animationS6l, duration*6);
-	   setTimeout(animationS7l, duration*7);
-	   setTimeout(animationS8l, duration*8);
-	 }
-	 static.ph = ch;
-	 static.pm = cm;
-	 static.ps = cs;
+	 //	 console.log('p='+ph+','+pm+','+ps);
+	 //	 console.log('c='+ch+','+cm+','+cs);
+	 
+	 if (ch != ph) $('.hourNumberU').delay(function() n1u(this, ph));
+	 if (cm != pm) $('.minuteNumberU').delay(function() n1u(this, pm));
+	 if (cs != ps) $('.secondNumberU').delay(function() n1u(this, ps));
+
+	 if (ch != ph) $('.hourNumberU').delay(function() n2u(this, ph));
+	 if (cm != pm) $('.minuteNumberU').delay(function() n2u(this, pm));
+	 if (cs != ps) $('.secondNumberU').delay(function() n2u(this, ps));
+							
+	 if (ch != ph) $('.hourNumberU').delay(function() n3u(this, ph));
+	 if (cm != pm) $('.minuteNumberU').delay(function() n3u(this, pm));
+	 if (cs != ps) $('.secondNumberU').delay(function() n3u(this, ps));
+
+	 if (ch != ph) $('.hourNumberU').delay(function() n4u(this, ch));
+	 if (cm != pm) $('.minuteNumberU').delay(function() n4u(this, cm));
+	 if (cs != ps) $('.secondNumberU').delay(function() n4u(this, cs));
+
+
+	 if (ch != ph) $('.hourNumberL').delay(function() n5l(this, ph));
+	 if (cm != pm) $('.minuteNumberL').delay(function() n5l(this, pm));
+	 if (cs != ps) $('.secondNumberL').delay(function() n5l(this, ps));
+
+
+	 if (ch != ph) $('.hourNumberL').delay(function() n6l(this, ph));
+	 if (cm != pm) $('.minuteNumberL').delay(function() n6l(this, pm));
+	 if (cs != ps) $('.secondNumberL').delay(function() n6l(this, ps));
+
+
+	 if (ch != ph) $('.hourNumberL').delay(function() n7l(this, ph));
+	 if (cm != pm) $('.minuteNumberL').delay(function() n7l(this, pm));
+	 if (cs != ps) $('.secondNumberL').delay(function() n7l(this, ps));
+
+
+	 if (ch != ph) $('.hourNumberL').delay(function() n8l(this, ch));
+	 if (cm != pm) $('.minuteNumberL').delay(function() n8l(this, cm));
+	 if (cs != ps) $('.secondNumberL').delay(function() n8l(this, cs));
+
+	 ph = ch;
+	 pm = cm;
+	 ps = cs;
+	 $.resume(20);
        }
-   });
+    });
 /*]]>*/
 </script>
 </center>
