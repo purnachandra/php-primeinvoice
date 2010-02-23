2d9ed11a3e761d8cb290969ea7e08dd685ba8bd7
mocapapa@g.pugpug.org <mocapapa@g.pugpug.org@fbabf7d8-ffad-11dd-802b-177316e4ac06>
2010-02-23 10:09:21 +0000

    changed widget collapse/expand application component as using icons

--- trunk/protected/components/Portlet.php
+++ trunk/protected/components/Portlet.php
@@ -13,8 +13,10 @@ class Portlet extends CWidget
 		if(!$this->visible)
 			return;
 		echo "<div class=\"{$this->cssClass}\">\n";
-		if($this->title!==null)
-			echo "<div class=\"{$this->headerCssClass}\">{$this->title}</div>\n";
+		if($this->title!==null) {
+			echo "<div class=\"{$this->headerCssClass}\" style=\"position:relative;\">{$this->title}\n";
+			echo "  <div class=\"expandButton\" style=\"width:18px;height:18px;top:2px;right:4px;background-image:url(".Yii::app()->request->baseUrl."/systemImages/widgetCollapseIcon.png);background-repeat:no-repeat;position:absolute;\" class=\"expandButton\"></div></div>\n";
+		}
 		echo "<div class=\"{$this->contentCssClass}\">\n";
 	}
 
